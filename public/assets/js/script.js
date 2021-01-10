95aeb7a5ed4beec33a47bff0828044c6b5616d57
Clem <valles.clement@gmail.com>
2021-01-09 18:53:16 -0600

    adding script.js file for onclick function

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,46 +1,41 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-sleep").on("click", function(event) {
+    $(".burger-devour-form").on("click", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+
       var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
-  
-      var newSleepState = {
-        sleepy: newSleep
-      };
   
       // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
-        type: "PUT",
-        data: newSleepState
-      }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+      console.log(burger_id);
+      $.ajax({
+        method: "PUT",
+        url: "/" + id
+      }).then(function(data) {
+        // reload page to display devoured burger in proper column
+        location.reload();
+      });
   
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
+      var newBurger = {
+        burger_name: $("#burger-name").val().trim(),
+        devoured: false
       };
   
       // Send the POST request.
-      $.ajax("/api/cats", {
+      $.ajax("/api/burgers", {
         type: "POST",
-        data: newCat
+        data: newBurger
       }).then(
         function() {
-          console.log("created new cat");
+          console.log("created new burger");
           // Reload the page to get the updated list
           location.reload();
         }
       );
     });
   });
-  
+});
