9feaf77af9ac867512fdbe7f9fd396925ea7367c
Clem <valles.clement@gmail.com>
2021-01-10 15:00:38 -0600

    Adding heroku updates

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,10 @@
 var mysql = require("mysql");
 
 //Connection to the database
-var connection;
-if(process.env.JAWSBD_URL){
-    connection = mysql.createConnection(process.env.JAWSBD_URL)
-}
-else {
-    var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
